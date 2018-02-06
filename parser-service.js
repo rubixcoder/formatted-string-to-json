e0acb7ff75c77dfd8075147f2c2dbb20643b669a
SurbhiHarsh <surbhi@infoobjects.com>
2018-02-06 13:12:04 +0530

    Removing the use of forEach loop

--- parser-service.js
+++ parser-service.js
@@ -18,35 +18,25 @@ class ParserService {
             parsedJson.id = delimitedData[1];
             parsedJson.dob = delimitedData[3];
             parsedJson.imageId = delimitedData[5];
-            var nameStringPattern = delimitedData[2].match(/\<([^\>]*)\>/g);
-            if (nameStringPattern && nameStringPattern.length == 3) {
-                var names = [];
-                nameStringPattern.forEach((item) => {
-                    item = item.substring(1, item.length - 1);
-                    names.push(item);
-                });
-                parsedJson.name.first = names[0];
-                parsedJson.name.middle = names[1];
-                parsedJson.name.last = names[2];
-            }
-            var locationStringPattern = delimitedData[4].match(/<((?:[^<>]|<<[^>]*>>)*)>/g);
-            if (locationStringPattern && locationStringPattern.length % 3 == 0) {
-                for (var i = 0; i < locationStringPattern.length; i += 3) {
-                    var location = [];
-                    var temp = locationStringPattern.slice(i, i + 3);
-                    temp.forEach((item) => {
-                        item = item.substring(1, item.length - 1);
-                        location.push(item);
-                    });
+            var names = delimitedData[2].split('<');
+            parsedJson.name.first = names[1].replace('>', '');
+            parsedJson.name.middle = names[2].replace('>', '');
+            parsedJson.name.last = names[3].replace('>', '');
+
+            var locations = delimitedData[4].split('>>');
+            locations.forEach((item) => {
+                if (item != "") {
+                    var location = item.split('<');
+                    console.log(location);
                     parsedJson.locations.push({
-                        name: location[0],
+                        name: location[1].replace('>', ''),
                         coords: {
-                            long: location[1],
-                            lat: location[2]
+                            long: location[3].replace('>', ''),
+                            lat: location[4].replace('>', '')
                         }
-                    })
+                    });
                 }
-            }
+            });
             return Promise.resolve(parsedJson);
         }
     }
